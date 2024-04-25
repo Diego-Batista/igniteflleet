f98144284387c3821230d46c05a93be988559de4
Diego-Batista <diegobtistadev@gmail.com>
2024-04-25 14:22:10 -0300

    Carregando dados do usuário

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -4,13 +4,17 @@ import { Container, Greeting, Message, Name, Picture } from './styles';
 import { Power } from 'phosphor-react-native';
 import { TouchableOpacity } from 'react-native';
 
+import { useUser } from '@realm/react';
+
 import theme from '../../theme';
 
 export function HomeHeader() {
+  const user = useUser();
+
   return (
     <Container>
       <Picture 
-        source={{ uri: 'https://github.com/rennand.png' }}
+        source={{ uri: user?.profile.pictureUrl }}
         placeholder='L184i9ofbHof00ayjsay~qj[ayj@'
       />
       <Greeting>
@@ -19,7 +23,7 @@ export function HomeHeader() {
         </Message>
 
         <Name>
-          Rodrigo
+           {user?.profile.name}
         </Name>
       </Greeting>
 
