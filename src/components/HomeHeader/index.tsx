0b6eed6e2fe5e2aefe5e9188d7636da0dc51b6f9
Diego-Batista <diegobtistadev@gmail.com>
2024-04-25 14:12:38 -0300

    Ícones

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Container, Greeting, Message, Name } from './styles';
 
+import { Power } from 'phosphor-react-native';
+import { TouchableOpacity } from 'react-native';
+
+import theme from '../../theme';
+
 export function HomeHeader() {
   return (
     <Container>
@@ -13,6 +18,10 @@ export function HomeHeader() {
           Rodrigo
         </Name>
       </Greeting>
+
+      <TouchableOpacity>
+        <Power size={32} color={theme.COLORS.GRAY_400} />
+      </TouchableOpacity>
 
     </Container>
   );
