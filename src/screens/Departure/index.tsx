efc72d4b3839fb0d122701aa921787ecd4d294b2
Diego-Batista <diegobtistadev@gmail.com>
2024-04-28 14:22:02 -0300

    Input Refs

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+import { TextInput } from 'react-native';
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { LicensePlateInput } from '../../components/LicensePlateInput';
@@ -5,6 +7,13 @@ import { TextAreaInput } from '../../components/TextAreaInput';
 import { Container, Content } from './styles';
 
 export function Departure() {
+
+  const descriptionRef = useRef<TextInput>(null);
+
+  function handleDepartureRegister() {
+    console.log('OK!');
+  }
+
   return (
     <Container>
       <Header title='Saída' />
@@ -13,15 +22,24 @@ export function Departure() {
         <LicensePlateInput 
           label='Placa do veículo' 
           placeholder="BRA1234"
+          onSubmitEditing={() => {
+            descriptionRef.current?.focus()
+          }}
+          returnKeyType='next'
         />
 
         <TextAreaInput 
+          ref={descriptionRef}
           label='Finalizade'
           placeholder='Vou utilizar o veículo para...'
+          onSubmitEditing={handleDepartureRegister}
+          returnKeyType='send'
+          blurOnSubmit
         />
 
         <Button 
           title='Registar Saída'
+          onPress={handleDepartureRegister}
         />
       </Content>
     </Container>
