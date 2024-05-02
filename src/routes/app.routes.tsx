cbf75abd9c05ef3f78c9891b4ebb9322aa48d159
Diego-Batista <diegobtistadev@gmail.com>
2024-05-02 14:06:28 -0300

    Criando a rota de registrar a chegada

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { Arrival } from '../screens/Arrival';
 import { Departure } from '../screens/Departure';
 import { Home } from '../screens/Home';
 
@@ -16,6 +17,11 @@ export function AppRoutes() {
       <Screen 
         name="departure"
         component={Departure}
+      />
+
+      <Screen 
+        name="arrival"
+        component={Arrival}
       />
     </Navigator>
   )
