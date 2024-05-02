4b38e286e135741da4c598c54a8bbf6868d790ed
Diego-Batista <diegobtistadev@gmail.com>
2024-05-02 14:25:29 -0300

    Criando o Button Icon

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -2,7 +2,9 @@ import { useRoute } from '@react-navigation/native';
 
 import { Container, Content, Description, Footer, Label, LicensePlate } from './styles';
 
+import { X } from 'phosphor-react-native';
 import { Button } from '../../components/Button';
+import { ButtonIcon } from '../../components/ButtonIcon';
 import { Header } from '../../components/Header';
 
 type RouteParamProps = {
@@ -39,6 +41,10 @@ export function Arrival() {
         </Description>
 
         <Footer>
+          <ButtonIcon 
+            icon={X} 
+          />
+          
           <Button title='Registrar chegada' />
         </Footer>
       </Content>
