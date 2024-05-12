1845dcfcb25fb7698ba1942e7175bdafe827da13
Diego-Batista <diegobtistadev@gmail.com>
2024-05-12 08:59:24 -0300

    Criando o componente LocationInfo

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -19,6 +19,7 @@ import { useNavigation } from '@react-navigation/native';
 import { useUser } from '@realm/react';
 
 import { Loading } from '../../components/Loading';
+import { LocationInfo } from '../../components/LocationInfo';
 import { useRealm } from '../../libs/realm';
 import { Historic } from '../../libs/realm/schemas/Historic';
 
@@ -27,6 +28,7 @@ export function Departure() {
   const [licensePlate, setLicensePlate] = useState('');
   const [isRegistering, setIsResgistering] = useState(false);
   const [isLoadingLocation, setIsLoadingLocation] = useState(true);
+  const [currentAddress, setCurrentAddress] = useState<string | null>(null)
 
   const [locationForegroundPermission, requestLocationForegroundPermission] = useForegroundPermissions();
 
@@ -88,7 +90,9 @@ export function Departure() {
     }, (location) => {
       getAddressLocation(location.coords)
       .then(address => {
-        console.log(address)
+        if(address) {
+          setCurrentAddress(address)
+        }
       })
       .finally(() => setIsLoadingLocation(false))
     }).then(response => subscription = response);
@@ -124,6 +128,13 @@ export function Departure() {
       <KeyboardAwareScrollView extraHeight={100}>
         <ScrollView>
           <Content>
+            {
+              currentAddress &&
+              <LocationInfo
+                label='Localização atual'
+                description={currentAddress}
+              />
+            }
             <LicensePlateInput
               ref={licensePlateRef}
               label='Placa do veículo'
