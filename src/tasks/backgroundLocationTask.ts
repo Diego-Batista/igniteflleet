0cb39f35fb5737b20f1b546f4fed8efff2063000
Diego-Batista <diegobtistadev@gmail.com>
2024-05-22 14:11:18 -0300

    Criando a função para parar a task

--- src/tasks/backgroundLocationTask.ts
+++ src/tasks/backgroundLocationTask.ts
@@ -1,4 +1,9 @@
-import { Accuracy, startLocationUpdatesAsync } from 'expo-location';
+import {
+    Accuracy,
+    hasStartedLocationUpdatesAsync,
+    startLocationUpdatesAsync,
+    stopLocationUpdatesAsync
+} from 'expo-location';
 import * as TaskManager from 'expo-task-manager';
 
 export const BACKGROUND_TASK_NAME =  'location-tracking';
@@ -25,6 +30,11 @@ TaskManager.defineTask(BACKGROUND_TASK_NAME, ({ data, error }: any) => {
 })
 
 export async function startLocationTask() {
+    const hasStarted = await hasStartedLocationUpdatesAsync(BACKGROUND_TASK_NAME)
+
+    if(hasStarted) {
+      await stopLocationTask();
+    }
     try {
       await startLocationUpdatesAsync(BACKGROUND_TASK_NAME, {
         accuracy: Accuracy.Highest,
@@ -35,3 +45,16 @@ export async function startLocationTask() {
       console.log(error)
     }
   }
+
+  export async function stopLocationTask(){
+    try {
+      const hasStarted = await hasStartedLocationUpdatesAsync(BACKGROUND_TASK_NAME)
+  
+      if(hasStarted) {
+        await stopLocationUpdatesAsync(BACKGROUND_TASK_NAME)
+      }
+  
+    } catch (error) {
+      console.log()
+    }
+ }
